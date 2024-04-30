a9c5a0597dee50598584bef21eb87d6389b3638b
Oscar Saravia <osaraviav13@gmail.com>
2024-04-29 22:53:28 -0400

    Proyecto - APP BOOKS: Validacion de datos y webShare agregados

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const URL_BOOKS = "https://potterapi-fedeperin.vercel.app/es/books";
-const TEMPLATE_CARD =  ({title, description}) => `
+const TEMPLATE_CARD =  ({title, description, cover}) => `
 <div class="demo-card-wide mdl-card mdl-shadow--2dp">
-<div class="mdl-card__title">
-    <h2 class="mdl-card__title-text" data-title="${title}" data-description="${description}" >${title}</h2>
+<div class="mdl-card__title" style="background: url('${cover}') center / cover">
+    <h2 class="mdl-card__title-text" data-title="${title}" data-description="${description}" data-url = "${cover}">${title}</h2>
 </div>
 <div class="mdl-card__supporting-text">
     ${description.substring(0, 100)}...
@@ -15,10 +15,10 @@ const TEMPLATE_CARD =  ({title, description}) => `
 </div>
 <div class="mdl-card__menu">
     <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-    <i class="material-icons">share</i>
+    <i class="material-icons" id="button_share">share</i>
     </button>
 </div>
-</div>;
+</div>
 `
 
 async function getBooks() {
@@ -37,7 +37,6 @@ const getLocalStorage = (key) => {
 
 // MAIN
 document.addEventListener("DOMContentLoaded", async function () {
-
 const ICON_SHOPPING = document.getElementById('shopping_cart')
 ICON_SHOPPING.setAttribute('data-badge', getLocalStorage('app_books_quantity') || 0) 
 const LIST_BOOKS = JSON.parse(getLocalStorage('app_books_details')) || []
@@ -56,6 +55,7 @@ const books = await getBooks();
   buttons.forEach( (button) => {
     const title = button.parentElement.parentElement.querySelector('.mdl-card__title-text').getAttribute('data-title')
     const description = button.parentElement.parentElement.querySelector('.mdl-card__title-text').getAttribute('data-description')
+
     button.addEventListener('click',  () => {
         let count = +ICON_SHOPPING.getAttribute('data-badge') || 0
         ICON_SHOPPING.setAttribute('data-badge', count+ 1)
@@ -65,8 +65,37 @@ const books = await getBooks();
             description: description,
             date: new Date().getTime()
         }
-        LIST_BOOKS.push(bookObject)
-        setLocalStorage('app_books_details', JSON.stringify(LIST_BOOKS))
+
+        const isValid = LIST_BOOKS.find( (book) => book.title == bookObject.title)
+        if (!isValid) {
+          LIST_BOOKS.push(bookObject)
+          setLocalStorage('app_books_details', JSON.stringify(LIST_BOOKS))
+        }
     })
   })
+
+  const btnShare = document.querySelectorAll('.mdl-button--icon')
+  btnShare.forEach( (btn) => {
+    const title = btn.parentElement.parentElement.querySelector('.mdl-card__title-text').getAttribute('data-title')
+    const description = btn.parentElement.parentElement.querySelector('.mdl-card__title-text').getAttribute('data-description')
+    const url = btn.parentElement.parentElement.querySelector('.mdl-card__title-text').getAttribute('data-url')
+
+    btn.addEventListener("click", async () => {
+      const shareData = {
+        title: title,
+        text: description,
+        url: url,
+      };
+      try {
+        await navigator.share(shareData);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  })
 });
+
+
+// TODO: Validar que en la cadena de valores no se repitan valores
+
+// TODO: usar WebShare
